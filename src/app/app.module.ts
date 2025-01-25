16bf68ce9d7f6bc3dfaf468713cb23af530986b7
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-25 22:43:25 +0200

    remove shared module imports from app and chatbot modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/chatbot/chatbot.module.ts
+++ src/app/modules/chatbot/chatbot.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChatbotRoutingModule } from './chatbot-routing.module';
-import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ChatbotRoutingModule, SharedModule],
+  imports: [CommonModule, ChatbotRoutingModule],
 })
 export class ChatbotModule {}
