09bd434c4ecbbf6924ce0e7639df29bbaa4cf970
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-25 19:22:39 +0200

    setup chat component design

--- src/app/modules/chatbot/components/chat/chat.component.ts
+++ src/app/modules/chatbot/components/chat/chat.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
 export class ChatComponent {
-
+  adjustTextareaHeight(event: any): void {
+    const textarea = event.target;
+    const lineHeight = 24;
+    const maxLines = 5;
+    const maxHeight = lineHeight * maxLines;
+    textarea.style.height = 'auto';
+    const newHeight = Math.min(textarea.scrollHeight, maxHeight);
+    textarea.style.height = newHeight + 'px';
+  }
 }
