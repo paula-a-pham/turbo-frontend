e5f3e8c28e9fc2ccedf7c510b46ad5db0fb30219
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-26 12:16:32 +0200

    initial design for home component

--- src/app/modules/chatbot/components/home/home.module.ts
+++ src/app/modules/chatbot/components/home/home.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ChatModule } from '../chat/chat.module';
+import { BotHeaderModule } from '../bot-header/bot-header.module';
+import { BotFooterModule } from '../bot-footer/bot-footer.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, ChatModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ChatModule,
+    BotHeaderModule,
+    BotFooterModule,
+  ],
 })
 export class HomeModule {}
