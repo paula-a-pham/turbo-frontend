3ee598cf5e38176d0468c5ad0203823e0394c365
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-28 01:08:02 +0200

    enable collapsing to home sidebar

--- src/app/modules/chatbot/components/home/home.component.ts
+++ src/app/modules/chatbot/components/home/home.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  sidebarCollapsed!: boolean;
+  constructor() {
+    this.sidebarCollapsed = false;
+  }
 }
