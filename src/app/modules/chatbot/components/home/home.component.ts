ab4bf3618a5b2082357c18c530ad01a13ece487c
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-28 13:08:41 +0200

    update responsiveness for home sidebar

--- src/app/modules/chatbot/components/home/home.component.ts
+++ src/app/modules/chatbot/components/home/home.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   sidebarCollapsed!: boolean;
+  viewportWidth!: number;
 
   constructor() {
     this.sidebarCollapsed = false;
+    this.viewportWidth = 0;
+  }
+
+  ngOnInit(): void {
+    this.viewportWidth = window.innerWidth;
+    if (this.viewportWidth <= 1024) {
+      this.sidebarCollapsed = true;
+    }
+    window.addEventListener('resize', this.onViewResize);
   }
 
   toggleSidebarCollapsing(): void {
     this.sidebarCollapsed = !this.sidebarCollapsed;
+  }
+
+  onViewResize = (): void => {
+    this.viewportWidth = window.innerWidth;
+    console.log('Viewport Width:', this.viewportWidth);
+  };
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.onViewResize);
   }
 }
