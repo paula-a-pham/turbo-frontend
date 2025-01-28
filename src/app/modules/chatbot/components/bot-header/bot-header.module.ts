93ea3043f0be275a5aaa1647b7dc3b972821a3f0
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-28 16:26:35 +0200

    add router module to header to fixt routing problem

--- src/app/modules/chatbot/components/bot-header/bot-header.module.ts
+++ src/app/modules/chatbot/components/bot-header/bot-header.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BotHeaderComponent } from './bot-header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BotHeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [BotHeaderComponent],
 })
 export class BotHeaderModule {}
