4a4abe2fc7af3ad08233816a055aa4a4cc8e1e7a
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-28 12:46:01 +0200

    handle home sidebar collapse and expand

--- src/app/modules/chatbot/components/home/home.component.ts
+++ src/app/modules/chatbot/components/home/home.component.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   sidebarCollapsed!: boolean;
+
   constructor() {
     this.sidebarCollapsed = false;
+  }
+
+  toggleSidebarCollapsing(): void {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
   }
 }
