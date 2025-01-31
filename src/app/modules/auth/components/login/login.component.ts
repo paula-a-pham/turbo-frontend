3e59265e61f7a4a2551392c1a9388dbbcfdc3e3c
Paula Ashraf <paula.a.pham@gmail.com>
2025-01-31 17:13:00 +0200

    create log in process functionality

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseAuthService } from '../../../../core/services/firebase/auth/firebase-auth.service';
+import { ILoginUser } from '../../../../shared/models/iuser';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  reactiveForm!: FormGroup;
 
+  constructor(
+    private formbuilder: FormBuilder,
+    private firebaseAuthService: FirebaseAuthService
+  ) {
+    this.initReactiveForm();
+  }
+
+  initReactiveForm(): void {
+    this.reactiveForm = this.formbuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+
+  get email() {
+    return this.reactiveForm.get('email');
+  }
+  get password() {
+    return this.reactiveForm.get('password');
+  }
+
+  getEmailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'This field required!';
+    }
+    if (this.email?.errors?.['email']) {
+      return 'Email syntax not valid!';
+    }
+    return '';
+  }
+  getPasswordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'This field required!';
+    }
+    if (this.password?.errors?.['minlength']) {
+      return 'Password should be at min 8 character!';
+    }
+    return '';
+  }
+
+  async createUserWithEmailAndPassword(): Promise<void> {
+    const oldUser: ILoginUser = this.reactiveForm.value as ILoginUser;
+    const user: User | null =
+      await this.firebaseAuthService.logInWithEmailAndPassword(oldUser);
+
+    if (user) {
+      console.log('Done');
+    } else {
+      console.log('Error');
+    }
+  }
 }
