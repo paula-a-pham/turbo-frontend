a6673ed3e4736d28a539719a7a669ae9fc7f5f5e
Paula Ashraf <paula.a.pham@gmail.com>
2025-02-02 16:04:06 +0200

    create auth guard

---
+++ src/app/core/guards/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { FirebaseAuthService } from '../../services/firebase/auth/firebase-auth.service';
+
+export const authGuard: CanActivateFn = async (route, state) => {
+  const firebaseAuthService = inject(FirebaseAuthService);
+  const router = inject(Router);
+
+  const user = await firebaseAuthService.getCurrentUser();
+  console.log('Auth state from promise:', user);
+
+  if (user) {
+    if (state.url.startsWith('/auth') || state.url.startsWith('/guest')) {
+      router.navigate(['/home'], { replaceUrl: true });
+      return false;
+    }
+    return true;
+  }
+
+  if (!user) {
+    if (state.url.startsWith('/home')) {
+      router.navigate(['/guest'], { replaceUrl: true });
+      return false;
+    }
+    return true;
+  }
+
+  return false;
+};
