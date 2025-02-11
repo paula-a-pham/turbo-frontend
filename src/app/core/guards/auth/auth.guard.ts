7fb034352f4289e0b1df17599c52e27036a40ce7
Paula Ashraf <paula.a.pham@gmail.com>
2025-02-11 14:46:48 +0200

    update firebase auth functions used inside auth guard and add some comments to it

--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,14 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { FirebaseAuthService } from '../../services/firebase/auth/firebase-auth.service';
+import { firstValueFrom } from 'rxjs';
 
 export const authGuard: CanActivateFn = async (route, state) => {
+  // inject needed services
   const firebaseAuthService = inject(FirebaseAuthService);
   const router = inject(Router);
 
-  const user = await firebaseAuthService.getCurrentUser();
+  // get current user
+  const user = await firstValueFrom(firebaseAuthService.getCurrentUser());
 
+  // check if user is authenticated
   if (user) {
+    // check if user opened auth or guest page and redirect him to home
     if (state.url.startsWith('/auth') || state.url.startsWith('/guest')) {
       router.navigate(['/home'], { replaceUrl: true });
       return false;
@@ -17,6 +22,7 @@ export const authGuard: CanActivateFn = async (route, state) => {
   }
 
   if (!user) {
+    // check if user opened home page and redirect him to guest
     if (state.url.startsWith('/home')) {
       router.navigate(['/guest'], { replaceUrl: true });
       return false;
