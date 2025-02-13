d39029095322bdf8d14cdc1166cc015af48daa17
Paula Ashraf <paula.a.pham@gmail.com>
2025-02-13 11:49:57 +0200

    update environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,5 @@ import { firebaseConfig } from './firebase-config';
 export const environment = {
   production: false,
   firebase: firebaseConfig,
+  groqApiUrl: 'https://api.groq.com/openai/v1/chat/completions',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@ import { firebaseConfig } from './firebase-config';
 export const environment = {
   production: true,
   firebase: firebaseConfig,
+  groqApiUrl: 'https://api.groq.com/openai/v1/chat/completions',
 };
