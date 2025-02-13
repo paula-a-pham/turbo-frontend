55f1ba14a672adb806787c1df32a7ca1d5fa8f31
Paula Ashraf <paula.a.pham@gmail.com>
2025-02-14 00:09:08 +0200

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 import { firebaseConfig } from './firebase-config';
+import { testGroqApiKey } from './groq-api-keys';
 
 export const environment = {
   production: true,
   firebase: firebaseConfig,
   groqApiUrl: 'https://api.groq.com/openai/v1/chat/completions',
+  groqApiKey: testGroqApiKey,
 };
